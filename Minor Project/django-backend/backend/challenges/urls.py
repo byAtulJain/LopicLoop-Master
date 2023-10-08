75127742821d5ee1659f6937a4b0d2f179ec748a
rahuldotjain <42427031+rahulj21@users.noreply.github.com>
2023-10-08 23:17:14 +0530

    HTML Inhertiance implementes/ URL corrected

--- Minor Project/django-backend/backend/challenges/urls.py
+++ Minor Project/django-backend/backend/challenges/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("learningResources", views.learningResources, name="learningResources"),
+    path("codeEditor", views.codeEditor, name="codeEditor"),
+    path("challenges", views.challenges, name="challenges"),
+    path("customRoom", views.customRoom, name="customRoom"),
+    path("login", views.login, name="login"),
+    path("contact", views.contact, name="contact"),
 ]
--- Minor Project/django-backend/backend/challenges/views.py
+++ Minor Project/django-backend/backend/challenges/views.py
@@ -11,3 +11,18 @@ def learningResources(request):
     data = ResourceCard.objects.all()
     context = {"data": data}
     return render(request, "pages/learningResources.html",context)
+
+def codeEditor(request):
+    return render(request, "pages/code editor.html")
+
+def customRoom(request):
+    return render(request, "pages/custom room.html")
+
+def challenges(request):
+    return render(request, "pages/challenges.html")
+
+def login(request):
+    return render(request, "pages/login signup.html")
+
+def contact(request):
+    return render(request, "pages/contact us.html")
